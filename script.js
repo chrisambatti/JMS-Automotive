8c2dcdbbdb5c5ad701b53a2197d92f1424794ff9
Chris <chrisambatti123@gmail.com>
2025-02-05 15:57:58 +0530

    commit 3

--- script.js
+++ script.js
@@ -32,3 +32,52 @@ form.addEventListener('submit',(e)=>{
 //     from_phone: "1234567890",
 //     message: "hello",
 //     });
+
+const countryCodes = [
+    { name: "United States", code: "+1" },
+    { name: "United Kingdom", code: "+44" },
+    { name: "Canada", code: "+1" },
+    { name: "Australia", code: "+61" },
+    { name: "India", code: "+91" },
+    { name: "Germany", code: "+49" },
+    { name: "France", code: "+33" }
+];
+
+const select = document.createElement("select");
+select.style.padding = "5px";
+select.style.fontSize = "16px";
+
+countryCodes.forEach(country => {
+    const option = document.createElement("option");
+    option.value = country.code;
+    option.textContent = `${country.name} (${country.code})`;
+    select.appendChild(option);
+});
+
+const input = document.getElementById("number");
+if (input) {
+    const wrapper = document.createElement("div");
+    wrapper.style.display = "flex";
+    wrapper.style.alignItems = "center";
+    wrapper.style.gap = "5px";
+
+    const countryCodeDisplay = document.createElement("span");
+    countryCodeDisplay.textContent = select.value;
+    countryCodeDisplay.style.fontSize = "16px";
+
+    select.addEventListener("change", () => {
+        countryCodeDisplay.textContent = select.value;
+    });
+
+    wrapper.appendChild(select);
+    wrapper.appendChild(countryCodeDisplay);
+    wrapper.appendChild(input);
+
+    input.parentNode.replaceChild(wrapper, input);
+} else {
+    console.error("Input field with ID 'number' not found.");
+}
+
+
+
+
